7ffc42f7f2264cca395c7e265a24a0859431c3df
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2022-10-16 17:45:08 +0300

    Add Week01 Lab01 Task03

---
+++ week-01/task-03/DataCenter.java
@@ -0,0 +1,58 @@
+public class DataCenter {
+
+    public static int getCommunicatingServersCount(int[][] map) {
+        int count = 0;
+        int[][] array = map.clone();
+        int n = array.length;
+        int m = array[0].length;
+
+        for (int i = 0; i < n; i++) {
+            int current = 0;
+            for (int j = 0; j < m; j++) {
+                if (array[i][j] > 0) {
+                    current++;
+                }
+            }
+
+            if (current < 2) {
+                continue;
+            }
+
+            count += current;
+            for (int j = 0; j < m; j++) {
+                if (array[i][j] == 1) {
+                    array[i][j] = 2;
+                }
+            }
+        }
+
+        for (int j = 0; j < m; j++) {
+            int current = 0;
+            for (int i = 0; i < n; i++) {
+                if (array[i][j] > 0) {
+                    current++;
+                }
+            }
+
+            if (current <= 2) {
+                continue;
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (array[i][j] == 1) {
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(getCommunicatingServersCount(new int[][] { { 1, 0 }, { 0,
+        // 1 } }));
+        // System.out.println(getCommunicatingServersCount(new int[][]{{1, 0}, {1,
+        // 1}}));
+        System.out.println(getCommunicatingServersCount(new int[][] { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } }));
+    }
+}
